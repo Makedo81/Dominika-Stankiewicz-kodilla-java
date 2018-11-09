9d4e41b0708fec003477f1586a35f3007f2af042
Makedo81 <domi.stan@wp.pl>
2018-11-09 13:26:36 +0100

    Calculator-task 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,25 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Modul6.Wprowadzenie do testowania oprogramowania");
+        System.out.println("Modul 6.Wprowadzenie do testowania oprogramowania");
+
+        Calculator result= new Calculator(50,20);
+
+        int addition = result.addAToB();
+        if (addition==(50+20)){
+            System.out.println ("Addition test positive");
+        }else {
+            System.out.println("Addition test error");
+        }
+
+        int substraction = result.substractAFromB();
+        if (substraction==(50-20)){
+            System.out.println ("Substraction test positive");
+        }else {
+            System.out.println("Substraction test error");
+        }
     }
 }
