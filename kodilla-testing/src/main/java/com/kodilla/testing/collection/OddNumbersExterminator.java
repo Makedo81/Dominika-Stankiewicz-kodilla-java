d9fb631dd283ef07bfcff100b092e6d99c3d2620
Makedo81 <domi.stan@wp.pl>
2018-11-22 19:40:41 +0100

    Task 6.3 brakujacy plik

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -3,15 +3,14 @@ import java.util.ArrayList;
 
 public class OddNumbersExterminator {
 
-    public ArrayList<Integer> exterminate(ArrayList<Integer> normalList) {
+    public ArrayList<Integer> exterminate(ArrayList<Integer> allNumbersList) {
 
-        ArrayList<Integer> evenNumbers=null;
-        for (int i = 0; i < normalList.size(); i++) {
+        ArrayList<Integer> evenNumbers= new ArrayList<>();
+        for (int i = 0; i < allNumbersList.size(); i++) {
 
-            int numberToExaminate = normalList.get(i);
+            int numberToExaminate = allNumbersList.get(i);
 
             if (numberToExaminate % 2 == 0) {
-                evenNumbers = new ArrayList<>();
                 evenNumbers.add(numberToExaminate);
             }
         }
